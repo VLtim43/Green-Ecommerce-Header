12672131f2d123fa1af7abcebe95e00cd3d987b4
fernando <fernandoreal.valentim@gmail.com>
2022-10-28 18:40:20 -0300

    .

--- src/components/AboutCard.tsx
+++ src/components/AboutCard.tsx
@@ -2,14 +2,15 @@ import React, { ReactNode } from "react";
 
 interface Props {
   Icon: ReactNode;
-  Title: String;
+  Title: string;
+  id: string;
 }
 
-const AboutCard = ({ Icon, Title }: Props) => {
+const AboutCard = ({ Icon, Title, id }: Props) => {
   return (
-    <div className="about-card">
+    <div className="about-card" id={id}>
       {Icon}
-      <h1 className="title">{Title}</h1>
+      <h2 className="title">{Title}</h2>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam, optio </p>
     </div>
   );
--- src/fragments/SectionAbout.tsx
+++ src/fragments/SectionAbout.tsx
@@ -5,9 +5,9 @@ export const SectionAbout = () => {
   return (
     <section id="about-section">
       <div id="row-div">
-        <AboutCard Icon={<FlowerLotus size={80} />} Title={"Organic only"} />
-        <AboutCard Icon={<Flask size={80} />} Title={"Lab Tested"} />
-        <AboutCard Icon={<Cat size={80} />} Title={"Cruelty Free"} />
+        <AboutCard Icon={<FlowerLotus size={80} />} Title={"Organic only"} id={"organic"} />
+        <AboutCard Icon={<Flask size={80} />} Title={"Lab Tested"} id={"lab"} />
+        <AboutCard Icon={<Cat size={80} />} Title={"Cruelty Free"} id={"animal"} />
       </div>
     </section>
   );
