279f4afcb24d84dc6407be2f5845875b1374e864
FernandoValentim43 <fernandoreal.valentim@gmail.com>
2022-11-10 18:53:51 -0300

    footer

--- src/fragments/SectionFooter.tsx
+++ src/fragments/SectionFooter.tsx
@@ -1,9 +1,22 @@
+const logo = "/assets/green.png";
+
+import * as Separator from "@radix-ui/react-separator";
 
 export const SectionFooter = () => {
   return (
     <section id="footer-section">
       <footer>
-        <p>oi</p>
+        {/*  <h2 className="copyright">2022 - Fernando Valentim</h2> */}
+        <div id="logo-div">
+          <img src={logo} />
+        </div>
+        
+        <Separator.Root
+          className="SeparatorRoot"
+          decorative
+          orientation="vertical"
+        />
+        
       </footer>
     </section>
   );
