c6abf9d1718dcbfbd55baf9fedc94b9b55875b94
FernandoValentim43 <fernandoreal.valentim@gmail.com>
2022-11-23 18:04:17 -0300

    footer socials

--- src/fragments/SectionFooter.tsx
+++ src/fragments/SectionFooter.tsx
@@ -1,14 +1,18 @@
 const logo = "/assets/green.png";
-const background = "../../../public/adobe/1000_F_216613855_khO3DLXshZ3ANkdIWE2usSnKSYCENcXx.jpg";
+const background =
+  "../../../public/adobe/1000_F_216613855_khO3DLXshZ3ANkdIWE2usSnKSYCENcXx.jpg";
 
 import * as Separator from "@radix-ui/react-separator";
-
+import { FacebookLogo, InstagramLogo, PinterestLogo } from "phosphor-react";
 
 export const SectionFooter = () => {
   return (
     <section id="footer-section">
       <footer>
-        <div className="main-footer"  style={{ backgroundImage: `url(${background})` }}>
+        <div
+          className="main-footer"
+          style={{ backgroundImage: `url(${background})` }}
+        >
           <div id="logo-div">
             <img src={logo} />
           </div>
@@ -18,9 +22,19 @@ export const SectionFooter = () => {
             decorative
             orientation="vertical"
           />
+
+          <div id="socials">
+            <InstagramLogo size={32} />
+            <FacebookLogo size={32} />
+            <PinterestLogo size={32} />
+          </div>
+
+          <div  id="teste">
+            <p>opi</p>
+          </div>
         </div>
 
-        <h3 className="copyright">© 2022 Fernando - All rights reserved. </h3>
+        <h3 className="copyright">© 2022 Fernando - All rights reserved </h3>
       </footer>
     </section>
   );
