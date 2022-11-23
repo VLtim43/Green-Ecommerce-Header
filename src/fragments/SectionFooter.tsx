203174a56e98b398b506677e64f2ab050683e2c6
FernandoValentim43 <fernandoreal.valentim@gmail.com>
2022-11-23 17:41:25 -0300

    fixed footer img path]

--- src/fragments/SectionFooter.tsx
+++ src/fragments/SectionFooter.tsx
@@ -1,12 +1,14 @@
 const logo = "/assets/green.png";
+const background = "../../../public/adobe/1000_F_216613855_khO3DLXshZ3ANkdIWE2usSnKSYCENcXx.jpg";
 
 import * as Separator from "@radix-ui/react-separator";
+
 
 export const SectionFooter = () => {
   return (
     <section id="footer-section">
       <footer>
-        <div className="main-footer">
+        <div className="main-footer"  style={{ backgroundImage: `url(${background})` }}>
           <div id="logo-div">
             <img src={logo} />
           </div>
