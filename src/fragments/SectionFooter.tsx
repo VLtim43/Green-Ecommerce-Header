3e6edc774287aa7904a2cacbe5fc05db6390328a
FernandoValentim43 <fernandoreal.valentim@gmail.com>
2022-12-03 13:22:52 -0300

    footerinfo

--- src/fragments/SectionFooter.tsx
+++ src/fragments/SectionFooter.tsx
@@ -3,7 +3,13 @@ const background =
   "../../../public/adobe/1000_F_216613855_khO3DLXshZ3ANkdIWE2usSnKSYCENcXx.jpg";
 
 import * as Separator from "@radix-ui/react-separator";
-import { FacebookLogo, InstagramLogo, PinterestLogo } from "phosphor-react";
+import {
+  BehanceLogo,
+  FacebookLogo,
+  InstagramLogo,
+  PinterestLogo,
+  TwitterLogo,
+} from "phosphor-react";
 
 export const SectionFooter = () => {
   return (
@@ -27,10 +33,25 @@ export const SectionFooter = () => {
             <InstagramLogo size={32} />
             <FacebookLogo size={32} />
             <PinterestLogo size={32} />
+            <TwitterLogo size={32} />
+            <BehanceLogo size={32} />
           </div>
 
-          <div  id="teste">
-            <p>opi</p>
+          <div className="info-container">
+            <div className="info" id="partnership">
+              <h3>Oi</h3>
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+
+            <div className="info" id="partnership">
+              <h3>Oi</h3>
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+
+            <div className="info" id="partnership">
+              <h3>Oi</h3>
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
           </div>
         </div>
 
